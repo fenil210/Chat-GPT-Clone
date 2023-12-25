877c9123ac82b619156a5e66f4a2c6186c87e208
fenil210 <121050723+fenil210@users.noreply.github.com>
2023-12-26 00:47:04 +0530

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,55 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import fetch from "node-fetch";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+const fn = (req, res) => {
+  res.send("Hello World!! This is fenil-GPT server");
+};
+
+app.get("/", fn);
+
+app.post("/api/chat", async (req, res) => {
+  const { messages } = req.body;
+  const data = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant. Add '(answer from fenil-gpt)' at the end of every answer gpt.",
+      },
+    ],
+  };
+
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        ...data,
+        messages: [...data.messages, ...messages],
+      }),
+    });
+    const json = await response.json();
+    console.log(json);
+    res.json({ question: messages, answer: json.choices });
+  }
+  catch (error) {
+    console.log(error, "error");
+  }
+});
+
+app.listen(8000, () => {
+  console.log("Server is running with full capability");
+});
